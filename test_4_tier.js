e01012ad086900990a6940ef4767c587cb1b25fb
trolidichvu <120279614+trolidichvu@users.noreply.github.com>
2023-04-13 22:50:01 +0700

    Update test_4_tier.js

--- test_4_tier.js
+++ test_4_tier.js
@@ -9,7 +9,7 @@ for (let i = 0; i < desiredArr.length; i++) {
   let temp = desired.split(',');
   if (temp[1] == 0) {
     for (let j = 1; j <= 12; j++) {
-      for (let rac = 0; rac <= 200; rac = rac +1 ) {
+      for (let rac = 0; rac <= 144; rac = rac +1 ) {
         if (temp[0] <= j)
           fs.appendFileSync("test.txt", desired + ' ' + j + ',0,0 ' + rac + '\n');
       }
@@ -17,7 +17,7 @@ for (let i = 0; i < desiredArr.length; i++) {
   } else if (temp[2] == 0) {
     for (let j = 1; j <= 12; j++) {
       for (let k = 1; k <= 12; k++) {
-        for (let rac = 0; rac <= 200; rac = rac+1) {
+        for (let rac = 0; rac <= 144; rac = rac+1) {
           if (temp[0] <= j && temp[1] <= k)
             fs.appendFileSync("test.txt", desired + ' ' + j + ',' + k + ',0 ' + rac + '\n');
         }
@@ -27,7 +27,7 @@ for (let i = 0; i < desiredArr.length; i++) {
     for (let j = 1; j <= 12; j++) {
       for (let k = 1; k <= 12; k++) {
         for (let l = 1; l <= 12; l++) {
-          for (let rac = 0; rac <= 200; rac = rac +1) {
+          for (let rac = 0; rac <= 144; rac = rac +1) {
             if (temp[0] <= j && temp[1] <= k && temp[2] <= l)
               fs.appendFileSync("test.txt", desired + ' ' + j + ',' + k + ',' + l + ' ' + rac + '\n');
           }
