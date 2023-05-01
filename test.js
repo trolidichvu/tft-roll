b99573efe73a2ff85a084da5233974f161a9dd32
trolidichvu <120279614+trolidichvu@users.noreply.github.com>
2023-05-01 17:19:08 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -4,12 +4,14 @@ let desiredArr = fs.readFileSync("./desired.txt", "utf-8").trim().split('\n');
 
 for (let i = 0; i < desiredArr.length; i++) {
   console.log(i)
-  let desired = desiredArr[i];
+  let desired = desiredArr[i].trim();
 
   let temp = desired.split(',');
+  for(let j=0; j<temp.length; j++) temp[j] =parseInt(temp[j]);
+
   if (temp[1] == 0) {
     for (let j = 1; j <= 18; j++) {
-      for (let rac = 0; rac <= 234; rac = rac +1 ) {
+      for (let rac = 0; rac <= 200; rac = rac +1 ) {
         if (temp[0] <= j)
           fs.appendFileSync("test.txt", desired + ' ' + j + ',0,0 ' + rac + '\n');
       }
@@ -17,7 +19,7 @@ for (let i = 0; i < desiredArr.length; i++) {
   } else if (temp[2] == 0) {
     for (let j = 1; j <= 18; j++) {
       for (let k = 1; k <= 18; k++) {
-        for (let rac = 0; rac <= 234; rac = rac+1) {
+        for (let rac = 0; rac <= 200; rac = rac+1) {
           if (temp[0] <= j && temp[1] <= k)
             fs.appendFileSync("test.txt", desired + ' ' + j + ',' + k + ',0 ' + rac + '\n');
         }
@@ -27,9 +29,11 @@ for (let i = 0; i < desiredArr.length; i++) {
     for (let j = 1; j <= 18; j++) {
       for (let k = 1; k <= 18; k++) {
         for (let l = 1; l <= 18; l++) {
-          for (let rac = 0; rac <= 234; rac = rac +1) {
-            if (temp[0] <= j && temp[1] <= k && temp[2] <= l)
+          for (let rac = 0; rac <= 200; rac = rac +1) {
+            if (temp[0] <= j && temp[1] <= k && temp[2] <= l){
               fs.appendFileSync("test.txt", desired + ' ' + j + ',' + k + ',' + l + ' ' + rac + '\n');
+            }
+              
           }
         }
       }
---
+++ xuLyFileCal.js
@@ -0,0 +1,34 @@
+const calFolder = './cal/';
+const outputFolder = './calOutput/'
+const fs = require('fs');
+
+const CaseLength = 43168569;
+let numberOfFiles =0;
+
+let arrAll = [];
+fs.readdirSync(calFolder).forEach(file => {
+  numberOfFiles++;
+  let arrCal = fs.readFileSync(calFolder+file,'utf-8').trim().split(",");
+  arrAll.push(arrCal);
+});
+
+
+
+let result = [];
+for(let i = 0; i< CaseLength ; i++){
+    let sum = 0;
+    let time = 0;
+    for(let j=0;j<arrAll.length;j++){
+        if(arrAll[j][i]!="undefined"&&arrAll[j][i]){
+            sum+= parseInt(arrAll[j][i]);
+            time++;
+        }
+    }
+    if(time>0){
+        result[i] = sum / time;
+    }else{
+        result[i] = undefined;
+    }
+}
+
+fs.writeFileSync(outputFolder+numberOfFiles, JSON.stringify(result));
